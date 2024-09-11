89f6a8839a83a3825e1ce3d047b5bc790d048fd8
Alberto Herrera Nieto <alberto.herrera.dev@gmail.com>
2024-09-11 15:36:29 -0600

    Se modifico el archivo .ignore y se suben los ejercicios

---
+++ src/Ejercicio1.java
@@ -0,0 +1,17 @@
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		
+		int valor= 3;
+		for (int i = 1; i <= valor; i++) {
+			for (int j = 1; j <= valor; j++) {
+				System.out.print(i);
+				System.out.println(j);	
+			}
+			
+		}
+
+	}
+	
+}
---
+++ src/Ejercicio2.java
@@ -0,0 +1,15 @@
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		int altura=6;
+		for (int i = altura; i > 1; i--) {
+			for (int j = 1; j < i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
